426b6ceaf135ddb706cda8865ed78f607e407ae8
George Yildiz <geoyi478@student.liu.se>
2016-11-10 16:59:53 +0100

    add teleportation for asteroids

--- core/src/com/mygdx/game/entity/Asteroid.java
+++ core/src/com/mygdx/game/entity/Asteroid.java
@@ -2,6 +2,7 @@ package com.mygdx.game.entity;
 
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
+import com.mygdx.game.AsteroidsGame;
 
 /**
  * Created by youto814 on 03/11/16.
@@ -17,4 +18,33 @@ public class Asteroid extends AbstractEntity {
     public EntityType GetEntityType() {
         return EntityType.ASTEROID;
     }
+
+    @Override
+    public void updatePosition(final float delta){
+        super.updatePosition(delta);
+
+        // outside left
+        if(this.getPosition().x < 0 - this.getSize().x){
+            Vector2 position = new Vector2(AsteroidsGame.GAME_WIDTH, this.getPosition().y);
+            this.setPosition(position);
+        }
+        // outside right
+        else if (this.getPosition().x > AsteroidsGame.GAME_WIDTH){
+            Vector2 position = new Vector2(0 - this.getSize().x , this.getPosition().y);
+            this.setPosition(position);
+        }
+        // outside top
+        else if(this.getPosition().y > AsteroidsGame.GAME_HEIGHT){
+            Vector2 position = new Vector2(this.getPosition().x, 0 - this.getSize().y);
+            this.setPosition(position);
+        }
+        // outside bottom
+        else if(this.getPosition().y < 0 - this.getSize().y){
+            Vector2 position = new Vector2(this.getPosition().x, AsteroidsGame.GAME_HEIGHT);
+            this.setPosition(position);
+        }
+
+    }
 }
+
+

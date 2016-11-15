1fd75fbe0b55ad21fffa6838a48a490dde261a54
Fredrik Wallström <frewa814@student.liu.se>
2016-11-15 09:27:02 +0100

    Rotation function added

--- core/src/com/mygdx/game/entity/Player.java
+++ core/src/com/mygdx/game/entity/Player.java
@@ -8,12 +8,31 @@ import com.badlogic.gdx.math.Vector2;
  */
 public class Player extends AbstractEntity {
 
+    private Sprite sprite;
+    private int angularSpeed;
+
     public Player(Sprite sprite, Vector2 position, Vector2 size, Vector2 velocity, Vector2 acceleration,
                   double mass) {
         super(sprite, position, size, velocity, acceleration, mass);
+        this.sprite = sprite;
         float newX = position.x - size.x/2;
         float newY = position.y - size.y/2;
         this.setPosition(new Vector2(newX, newY));
+    }
+
+    public void rotateLeft(float delta){
+        angularSpeed = 5;
+        float angle = angularSpeed * delta;
+        sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
+        sprite.setRotation(angle);
+    }
+
+    public void rotateRight(float delta){
+        angularSpeed = -5;
+        float angle = angularSpeed * delta;
+        sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
+        sprite.setRotation(angle);
+
     }
 
     @Override
